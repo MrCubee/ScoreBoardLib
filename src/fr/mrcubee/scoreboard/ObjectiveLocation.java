fb47c4a6335dde450f6b62b8a322eef94870c43f
MrCubee <contact@mrcubee.fr>
2020-10-09 20:48:17 +0200

    [remove] useless ScoreBoardManager class and useless getLocation() method in ObjectiveLocation enumeration

--- src/fr/mrcubee/scoreboard/ObjectiveLocation.java
+++ src/fr/mrcubee/scoreboard/ObjectiveLocation.java
@@ -1,13 +1,7 @@
 package fr.mrcubee.scoreboard;
 
 public enum ObjectiveLocation {
-
     LIST,
     SIDEBAR,
     BELOW_NAME;
-
-    public int getLocation() {
-        return this.ordinal();
-    }
-
 }
