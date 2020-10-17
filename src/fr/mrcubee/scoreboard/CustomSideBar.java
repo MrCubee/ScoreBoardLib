a12c4b68a2145362e482e82e6a21c10fb1086cab
MrCubee <contact@mrcubee.fr>
2020-10-17 12:28:43 +0200

    [add] setLines method in CustomSideBar class
    To add multiple lines in a single call to this class.

--- src/fr/mrcubee/scoreboard/CustomSideBar.java
+++ src/fr/mrcubee/scoreboard/CustomSideBar.java
@@ -1,22 +1,24 @@
 package fr.mrcubee.scoreboard;
 
-import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class CustomSideBar {
 
-    private Objective objective;
-    private Map<Integer, Integer> hashs;
-    private Map<Integer, String> lines;
+    private final Objective objective;
+    private final Map<Integer, Integer> hash;
+    private final Map<Integer, String> lines;
+    private int oldSize;
 
     protected CustomSideBar(String name, String displayName) {
         this.objective = Objective.create(name, displayName);
-        this.hashs = new HashMap<Integer, Integer>();
+        this.hash = new HashMap<Integer, Integer>();
         this.lines = new HashMap<Integer, String>();
+        this.oldSize = 0;
     }
 
     public ConcurrentLinkedQueue<OfflinePlayer> getReceivers() {
@@ -26,14 +28,14 @@ public class CustomSideBar {
     public boolean setLine(int number, String line) {
         if (line == null)
             return false;
-        else if (this.hashs.containsKey(number) && this.hashs.get(number) == line.hashCode())
+        else if (this.hash.containsKey(number) && this.hash.get(number) == line.hashCode())
             return true;
         if (this.lines.containsKey(number)) {
             this.objective.removeScore(this.lines.remove(number));
-            this.hashs.remove(number);
+            this.hash.remove(number);
         }
         this.lines.put(number, line);
-        this.hashs.put(number, line.hashCode());
+        this.hash.put(number, line.hashCode());
         return this.objective.setScore(line, number);
     }
 
@@ -44,10 +46,27 @@ public class CustomSideBar {
     public boolean removeLine(int number) {
         String line = this.lines.remove(number);
 
-        this.hashs.remove(number);
+        this.hash.remove(number);
         if (line == null)
             return false;
         return this.objective.removeScore(line);
+    }
+
+    public void setLines(Collection<String> lines) {
+        int size;
+        int currentSize;
+
+        if (lines == null || lines.isEmpty())
+            currentSize = 0;
+        else {
+            size = lines.size();
+            currentSize = size;
+            for (String line : lines)
+                setLine(size--, line);
+        }
+        for (int i = currentSize + 1; i <= this.oldSize; i++)
+            removeLine(i);
+        this.oldSize = currentSize;
     }
 
     public boolean setDisplayName(String displayName) {
